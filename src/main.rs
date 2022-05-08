ba35f06091c7b7433d2018fc08c71844ee3a476e
Spirus10 <superbay11@gmail.com>
2022-05-07 17:02:12 -0700

    Update main.rs
    Had sync issues between remote and local - fun fun

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,21 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
+mod vga_buffer;
+
 use core::panic::PanicInfo;
 
 // This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello World!";
-
 #[no_mangle] // don't mangle the name of this function
 pub extern "C" fn _start() -> ! {
-    let vga_buffer = 0xb8000 as *mut u8; // casting `0xb8000` into a raw pointer
+    println!("Hello World{}", "!");
 
-    for (i, &byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // writes the string byte
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // writes the corresponding color byte; `0xb` is light cyan
-        }
-    }
-
-    // this function is the entry point, since the linker looks for a function
-    // named `_start` by default
     loop {}
 }
 
