207432973ba3efe0474fd7e1390e4434329e07f4
Spirus10 <superbay11@gmail.com>
2022-05-07 17:05:46 -0700

    typo

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use core::panic::PanicInfo;
 // This function is called on panic
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    println!("{}", info);
+    println!("{}", _info);
     loop {}
 }
 
